579153175a6566532db3b5ede90fc64b8b3d4a5b
nmiljanic <nmiljanic@unipu.hr>
2025-03-25 00:26:15 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-emerald-950 to-indigo-950 text-teal-100">
+      <div class="flex items-center mb-8">
+        <div class="text-6xl font-normal mr-2">
+          Pozdrav, ja sam
+        </div>
+        <div class="text-6xl font-bold text-cyan-400">
+          Niko Miljanić
+        </div>
+      </div>
+  
+      <div class="w-[1024px] border-b-2 border-teal-100/75 rounded-full mb-4"></div>
+  
+      <div class="w-[1024px] text-left text-[1.5rem] mb-8 text-teal-100/75">
+        Moji omiljeni filmovi:
+      </div>
+  
+      <div class="flex space-x-4">
+        <div class="w-[320px] h-[480px] bg-gray-200 rounded-[6px] overflow-hidden">
+          <img class="w-full h-full object-cover" src="https://lh6.googleusercontent.com/proxy/GBnoaospa6cSUo5vaxPocFFDOfjeBD9T1IbUlLf9VBt7dsJSw9V0613tvlFU_EaZPyuZxzNEvsdBK-36TvcTXiyO" alt="Poster 1">
+        </div>
+        <div class="w-[320px] h-[480px] bg-gray-200 rounded-[6px] overflow-hidden">
+          <img class="w-full h-full object-cover" src="https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p25765_p_v12_aj.jpg" alt="Poster 2">
+        </div>
+        <div class="w-[320px] h-[480px] bg-gray-200 rounded-[6px] overflow-hidden">
+          <img class="w-full h-full object-cover" src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_FMjpg_UX1000_.jpg" alt="Poster 3">
+        </div>
+      </div>
+      <div class="flex space-x-4 mt-4">
+        <div class="w-80 text-center text-xl text-teal-100/75">
+          Anora
+        </div>
+        <div class="w-80 text-center text-xl text-teal-100/75">
+          Scary Movie
+        </div>
+        <div class="w-80 text-center text-xl text-teal-100/75">
+            Avengers: Endgame
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  
+
+<style scoped>
+
+</style>
